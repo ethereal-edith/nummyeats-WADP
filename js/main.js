402da940c5ea1921494942c390c1119e96fa0a7f
Edith Agai <email.02yt@gmail.com>
2026-06-28 22:38:38 +0300

    feat: added reservation toast

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // contact form
 const contactForm = document.getElementById('contactForm');
-if (contactForm) {
+if (contactForm) { //prevents errors
     contactForm.addEventListener('submit', function(e) {
         e.preventDefault();
 
@@ -18,5 +18,24 @@ if (contactForm) {
 }
 
 // reservations storage
+const reservationForm = document.getElementById('reservationForm');
+if (reservationForm){ //prevents errors
+    reservationForm.addEventListener('submit', function(e){
+        e.preventDefault();
+ 
+        const name = document.getElementById('reservationName').value.trim();
+        const phone = document.getElementById('reservationPhone').value.trim();
+        const branch = document.getElementById('reservationBranch').value.trim();
+        const date = document.getElementById('reservationDate').value.trim();
+        const time = document.getElementById('reservationTime').value.trim();
+        const guests = document.getElementById('reservationGuests').value.trim();
+
+        const toastEl = document.getElementById('reservationToast');
+        const toast = new bootstrap.Toast(toastEl);
+        toast.show();
+
+        reservationForm.reset();
+    });
+}
 // reviews storage
 // menu search
